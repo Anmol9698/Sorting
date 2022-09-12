e5258c08c6f80ff03adc17c4af5784a856bdb68a
Your Name <anmoldeep0911@gmail.com>
2022-09-12 12:09:29 +0530

    BubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -29,7 +29,7 @@ class BubbleSort {
     int[] data = { -2, 45, 0, 11, -9 };
     
     // call method using class name
-    Main.bubbleSort(data);
+    BubbleSort.bubbleSort(data);
     
     System.out.println("Sorted Array in Ascending Order:");
     System.out.println(Arrays.toString(data));
